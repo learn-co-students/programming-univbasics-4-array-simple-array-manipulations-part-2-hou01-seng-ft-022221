9631c6ef7fa7c4e74ee74f4fb8abebcea5dcb757
Shane R <imspammails@gmail.com>
2021-01-22 11:53:58 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+def using_concat(first_array, second_array)
+  first_array.concat(second_array)
+end
+
+def using_insert(array, new_element)
+  #binding.pry
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, location)
+  array.delete_at(location)
+end
