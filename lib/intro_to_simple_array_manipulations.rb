37098d754dac12c0fb60ea05551841f21279b6b1
Allen Palacios <apalacios1994@gmail.com>
2021-02-16 04:48:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+require "pry"
+
+def using_concat(my_favorite_things, more_favs)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  
+  @all_my_favs = my_favorite_things.concat(more_favs)
+  
+  @my_favorite_things.push("sports cars", "flatiron school")
+end
+
+def using_insert(list_of_programming_languages, another_language)
+   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+   @another_language = "Python"
+   
+   @new_array = @list_of_programming_languages.insert(-4, @another_language)
+ end
+ 
+ def using_uniq(haircuts)
+   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+   
+   new_array = haircuts.uniq
+ end
+ 
+ def using_flatten(instruments)
+   instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+   flat_array = instruments.flatten
+ end
+ 
+ instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+ def using_delete(instructors, string)
+   no_offense_steven = instructors.delete("Steven")
+   instructors
+   end
+instructors
+
+famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+def using_delete_at(famous_robots, integer)
+  delete_robocop = famous_robots.delete_at(2)
+  delete_robocop
+end
+  
+  
+  
+  
+  
+  
+  
+  
